7c86bbaf90ca78c82e949b182cdf1ee6223b280c
Arin <arinmandal40@gmail.com>
2023-06-17 23:26:57 +0530

    Data-card(v2) Modified

--- Date-Counter/src/App.jsx
+++ Date-Counter/src/App.jsx
@@ -13,18 +13,32 @@ function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  const date = new Date("june 21 2027");
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+  const date = new Date("June 15 2023");
   date.setDate(date.getDate() + count);
   return (
     <div>
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
         <span>Step: {step}</span>
         <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
 
@@ -38,6 +52,11 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
