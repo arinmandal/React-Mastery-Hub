f54e76176c7a39b93ad268617d1c10289ef25eeb
Arin <arinmandal40@gmail.com>
2023-12-17 20:45:36 +0530

    bgColorChange

---
+++ bgColorChange/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+function App() {
+  const [color, setColor] = useState("black");
+  return (
+    <div
+      className='w-full h-screen flex justify-center'
+      style={{ backgroundColor: color }}
+    >
+      <div className='allButtons flex gap-1 bg-black rounded-md p-1 w-[700px] justify-between absolute bottom-28 my-0 mx-auto font-bold'>
+        <button
+          className='bg-red-500 px-7 rounded-md py-2'
+          onClick={() => setColor("red")}
+        >
+          RED
+        </button>
+        <button
+          className='bg-green-500 px-7 rounded-md py-2'
+          onClick={() => setColor("green")}
+        >
+          GREEN
+        </button>
+        <button
+          className='bg-yellow-500 px-7 rounded-md py-2'
+          onClick={() => setColor("yellow")}
+        >
+          YELLOW
+        </button>
+        <button
+          className='bg-purple-500 px-7 rounded-md py-2'
+          onClick={() => setColor("purple")}
+        >
+          PURPLE
+        </button>
+        <button
+          className='bg-blue-500 px-7 rounded-md py-2'
+          onClick={() => setColor("blue")}
+        >
+          BLUE
+        </button>
+        <button
+          className='bg-orange-500 px-7 rounded-md py-2'
+          onClick={() => setColor("orange")}
+        >
+          ORANGE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
