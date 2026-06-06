9c36084cdbdc4f5ff94e3d1dab19c38af1154e3c
Arin Mandal <arinmandal40@gmail.com>
2026-06-06 23:50:43 +0530

    feat: reorganize components in App to improve structure and readability

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import Greet from "./components/Greet";
+import Heading from "./components/Heading";
+import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import Status from "./components/Status";
 const App = () => {
 	const personName = {
 		first: "Bruce",
@@ -25,9 +28,16 @@ const App = () => {
 
 	return (
 		<div className='App'>
-			<Greet name='Alex' messageCount={10} isLoggedIn={true} />
+			{/* <Greet name='Alex' messageCount={10} isLoggedIn={true} /> */}
 			<Person name={personName} />
 			<PersonList names={nameList} />
+			<Status status='error' />
+			<Heading>Placeholder text</Heading>
+			<Oscar>
+				<Heading>Oscar goes to Leonardo Dicpario!</Heading>
+			</Oscar>
+
+			<Greet name='Alex' isLoggedIn={true}/>
 		</div>
 	);
 };
