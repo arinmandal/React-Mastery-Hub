0628c9478dad10af4cce77fb9f3a419671c24e78
Arin Mandal <arinmandal40@gmail.com>
2026-06-06 23:50:30 +0530

    feat: make messageCount prop optional in Greet component (Optional Props)

--- react-typescript/src/components/Greet.tsx
+++ react-typescript/src/components/Greet.tsx
@@ -1,14 +1,15 @@
 type GreetProps = {
 	name: string;
-	messageCount: number;
+	messageCount?: number;
 	isLoggedIn: boolean;
 };
 const Greet = (props: GreetProps) => {
+	const { messageCount = 0 } = props;
 	return (
 		<div>
 			<h2>
 				{props.isLoggedIn
-					? `Good to see you, ${props.name}! 🌟 You’ve got ${props.messageCount} updates
+					? `Good to see you, ${props.name}! 🌟 You’ve got ${messageCount} updates
 			waiting.`
 					: "Welcome Guest!"}
 			</h2>
